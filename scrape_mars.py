d58d3a35efcd26b67b0659d0880e5929e7349368
Kevin Avery <Averykev@gmail.com>
2020-09-03 12:36:14 -0700

    complete python code and unfinished app.py

---
+++ scrape_mars.py
@@ -0,0 +1,145 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+def init_browser():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+
+    ## NASA MARS NEWS ##
+
+    browser = init_browser()
+
+    url = 'https://mars.nasa.gov/news/'
+
+    browser.visit(url)
+
+    time.sleep(2)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    results = soup.find_all('div', class_='content_title')
+
+    results_2 = soup.find_all('div', class_='article_teaser_body')
+
+    title = results.find('a').text
+
+    paragraph = results_2.text
+
+
+
+    ##  JPL MARS SPACE IMAGES - FEATURED IMAGE  ##
+
+    image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+
+    browser.visit(image_url)
+
+    time.sleep(2)
+
+    browser.click_link_by_partial_text('FULL IMAGE')
+
+    time.sleep(2)
+
+    browser.click_link_by_partial_text('more info')
+
+    time.sleep(2)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    mars_image = soup.find('figure', class_='lede')
+
+    mars_image = mars_image.a['href']
+
+    jpl_base_url = 'https://www.jpl.nasa.gov'
+
+    featured_image_url = jpl_base_url + mars_image
+
+
+
+    ##  MARS FACTS TABLE  ##
+
+    mars_facts_url = 'https://space-facts.com/mars/'
+
+    tables = pd.read_html(mars_facts_url)
+
+    mars_df = tables[0]
+
+    mars_df.columns=['Measurment', 'Value']
+
+    html_table = mars_df.to_html()
+
+    html_table.replace('\n', '')
+
+    mars_table = mars_df.to_html('Mars_Facts_Table.html')
+
+
+
+    ##  MARS HEMISPHERES  ##
+
+    hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    browser.visit(hemi_url)
+
+    time.sleep(2)
+
+    html = browser.html
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    results = soup.find('div', class_='collapsible results')
+
+    unique_hemispheres = results.find_all('div', class_='item')
+
+    hemisphere_names = []
+
+    hemi_base_url = 'https://astrogeology.usgs.gov'
+
+    for hemi in unique_hemispheres:
+        title = hemi.find('h3').text
+    
+        partial_url = hemi.find('a', class_='itemLink product-item')['href']
+    
+        full_url = hemi_base_url + partial_url
+    
+        browser.visit(full_url)
+
+        time.sleep(1)
+    
+        html = browser.html
+    
+        soup = BeautifulSoup(html, 'html.parser')
+    
+        image_url = soup.find('img', class_='wide-image')['src']
+    
+        full_image_url = hemi_base_url + image_url
+    
+        hemisphere_names.append({'title': title, 
+                                'img_url': full_image_url})
+
+
+
+    ##  STORE DATA  ##
+
+    mars_info = {
+
+        "news_title": title,
+        "news_paragraph": paragraph,
+        "jpl_featured_image": featured_image_url,
+        "mars_table": mars_table,
+        "hemisphere_info": hemisphere_names
+
+    }
+
+    browser.quit()
+
+    return mars_info
+
